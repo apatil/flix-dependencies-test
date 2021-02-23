529dae3e21d38b81143a2e4fbf405d3443d05fe8
Anand Patil <appatil@pm.me>
2021-02-23 09:01:42 -0500

    Updated to working version

--- test.sh
+++ test.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-export CLASSPATH=$(pwd)/jars/commons-cli-1.4.jar
-# export CLASSPATH=$(pwd)/jars
-
-echo "Classpath is: ${CLASSPATH}"
-
-java -classpath "${CLASSPATH}" -jar /home/anand/.config/Code/User/globalStorage/flix.flix/flix.jar check --verbose
+java -cp "$(pwd)/jars/commons-cli-1.4.jar:/home/anand/.config/Code/User/globalStorage/flix.flix/flix.jar" ca.uwaterloo.flix.Main check --verbose
